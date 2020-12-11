043888c01dd26555176f6379ffb89d5bbc294a9e
Connor Ferguson <cpfergus1@gmail.com>
2020-12-11 11:13:25 -0700

    fixed silly testing mistakes on Service spec and facade spec - NO NEW CODE

--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -1,9 +1,9 @@
-# require 'rails_helper'
-# describe 'Seach Facade' do
-#   it 'returns characters' do
-#     characters = SearchFacade.characters('Fire Nation')
-#     expect(characters).to be_an(Array)
-#     expect(characters.size).to be_an(20)
-#     expect(characters.first).to be_an_instance_of(Character)
-#   end
-# end
+require 'rails_helper'
+describe 'Seach Facade' do
+  it 'returns characters' do
+    characters = SearchFacade.characters('Fire Nation')
+    expect(characters).to be_an(Array)
+    expect(characters.size).to eq(20)
+    expect(characters.first).to be_an_instance_of(Character)
+  end
+end
--- spec/services/search_spec.rb
+++ spec/services/search_spec.rb
@@ -1,16 +1,14 @@
-# require 'rails_helper'
-# describe 'SearchService' do
-#   it 'can reture air bender characters' do
-#     response = SearchService.character_data_response('Fire Nation')
-#     expect(response).to be_an(Array)
-#     expect(response[0]).to be_a(Hash)
-#
-#     expect(response[0][:allies].to be_a(Array))
-#     expect(response[0][:enemies].to be_a(Array))
-#     expect(response[0][:name].to be_a(String))
-#     expect(response[0][:affiliation].to be_a(String))
-#     unless response[0][:photoUrl].nil?
-#       expect(response[0][:photoUrl].to be_a(String))
-#     end
-#   end
-# end
+require 'rails_helper'
+describe 'SearchService' do
+  it 'can reture air bender characters' do
+    response = SearchService.character_data_response('Fire Nation')
+    expect(response).to be_an(Array)
+    expect(response[0]).to be_a(Hash)
+
+    expect(response[0][:allies]).to be_an(Array)
+    expect(response[0][:enemies]).to be_an(Array)
+    expect(response[0][:name]).to be_an(String)
+    expect(response[0][:affiliation]).to be_an(String)
+    expect(response[1][:photoUrl]).to be_an(String)
+  end
+end
